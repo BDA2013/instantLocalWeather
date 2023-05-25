bcba5c843285fd13a650ff465192acf5e9da904a
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-05-25 14:21:23 -0400

    added icons area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,28 @@
 // Example code to update the current day forecast
 document.getElementById("currentTemperature").innerText = "25°C";
 document.getElementById("currentCondition").innerText = "Sunny";
+document.getElementById("currentIcon");
 
 // Example code to update the 5-day forecast
 document.getElementById("day1Temperature").innerText = "23°C";
 document.getElementById("day1Condition").innerText = "Partly Cloudy";
+document.getElementById("day1Icon");
+
 document.getElementById("day2Temperature").innerText = "21°C";
 document.getElementById("day2Condition").innerText = "Rainy";
+document.getElementById("day2Icon");
+
 document.getElementById("day3Temperature").innerText = "22°C";
 document.getElementById("day3Condition").innerText = "Cloudy";
+document.getElementById("day3Icon"); // Example icon
+
 document.getElementById("day4Temperature").innerText = "24°C";
 document.getElementById("day4Condition").innerText = "Sunny";
+document.getElementById("day4Icon") // Example icon
+
 document.getElementById("day5Temperature").innerText = "26°C";
 document.getElementById("day5Condition").innerText = "Mostly Sunny";
+document.getElementById("day5Icon");
 
 function gatherWeather(lat, lon) {
   fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=89d3bde90b46aeb52fc0fca5cffee20e`)
