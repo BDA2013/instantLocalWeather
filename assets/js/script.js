6a9907e48167731d1bf37f1dab932eb5abc6ff1f
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-05-31 18:33:50 -0400

    Added data to Current Day

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,39 +4,39 @@ var weatherButton = document.getElementById("getWeather");
 
 // Example code to update the current day forecast
 var currentIcon = document.getElementById("currentIcon");
-var currentTemp = document.getElementById("currentTemperature").innerText = "25°C";
-var currentCond = document.getElementById("currentCondition").innerText = "Sunny";
+var currentTemp = document.getElementById("currentTemperature");
+var currentCond = document.getElementById("currentCondition");
 var currentWind = document.getElementById("currentWindSpeed");
 var currentHumi = document.getElementById("currentHumidity");
 
 // Example code to update the 5-day forecast
 var day1Icon = document.getElementById("day1Icon");
-var day1Temp = document.getElementById("day1Temperature").innerText = "23°C";
-var day1Cond = document.getElementById("day1Condition").innerText = "Partly Cloudy";
+var day1Temp = document.getElementById("day1Temperature");
+var day1Cond = document.getElementById("day1Condition");
 var day1Wind = document.getElementById("day1WindSpeed");
 var day1Humi = document.getElementById("day1Humidity");
 
 var day2Icon = document.getElementById("day2Icon");
-var day2Temp = document.getElementById("day2Temperature").innerText = "21°C";
-var day2Cond = document.getElementById("day2Condition").innerText = "Rainy";
+var day2Temp = document.getElementById("day2Temperature");
+var day2Cond = document.getElementById("day2Condition");
 var day2Wind = document.getElementById("day2WindSpeed");
 var day2Humi = document.getElementById("day2Humidity");
 
 var day3Icon = document.getElementById("day3Icon");
-var day3Temp = document.getElementById("day3Temperature").innerText = "22°C";
-var day3Cond = document.getElementById("day3Condition").innerText = "Cloudy";
+var day3Temp = document.getElementById("day3Temperature");
+var day3Cond = document.getElementById("day3Condition");
 var day3Wind = document.getElementById("day3WindSpeed");
 var day3Humi = document.getElementById("day3Humidity");
 
 var day4Icon = document.getElementById("day4Icon");
-var day4Temp = document.getElementById("day4Temperature").innerText = "24°C";
-var day4Cond = document.getElementById("day4Condition").innerText = "Sunny";
+var day4Temp = document.getElementById("day4Temperature");
+var day4Cond = document.getElementById("day4Condition");
 var day4Wind = document.getElementById("day4WindSpeed");
 var day4Humi = document.getElementById("day4Humidity");
 
 var day5Icon = document.getElementById("day5Icon");
-var day5Temp = document.getElementById("day5Temperature").innerText = "26°C";
-var day5Cond = document.getElementById("day5Condition").innerText = "Mostly Sunny";
+var day5Temp = document.getElementById("day5Temperature");
+var day5Cond = document.getElementById("day5Condition");
 var day5Wind = document.getElementById("day5WindSpeed");
 var day5Humi = document.getElementById("day5Humidity");
 
@@ -47,6 +47,10 @@ function gatherWeather(lat, lon) {
   })
   .then(function (data) {
     console.log(data);
+    currentTemp.innerHTML = parseInt(data.list[0].main.temp) + "°F";
+    currentCond.innerHTML = data.list[0].weather[0].description;
+    currentWind.innerHTML = parseInt(data.list[0].wind.speed) + " MPH";
+    currentHumi.innerHTML = data.list[0].main.humidity + "%" 
   });
 };
 
